673b2bb6f717c358f620a8084cb4b2a94e5108e2
kytan2992 <kytan2992@gmail.com>
2025-04-05 18:45:30 +0800

    added lambda functions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,13 +1,13 @@
 data "archive_file" "create_url" {
   type        = "zip"
-  source_dir  = "./lambda_functions/create_url"
-  output_path = "./lambda_functions/create_url.zip"  
+  source_dir  = "${path.module}/lambda_functions/create_url"
+  output_path = "${path.module}/lambda_functions/create_url.zip"  
 }
 
 data "archive_file" "retrieve_url" {
   type        = "zip"
-  source_dir  = "./lambda_functions/retrieve_url"
-  output_path = "./lambda_functions/retrieve_url.zip"  
+  source_dir  = "${path.module}/lambda_functions/retrieve_url"
+  output_path = "${path.module}/lambda_functions/retrieve_url.zip"  
 }
 
 resource "aws_lambda_function" "create_url" {
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "create_url" {
   handler       = "create_url.lambda_handler"
   runtime       = "python3.13"
   role          = aws_iam_role.lambda_role.arn
-  filename      = "./lambda_functions/create_url.zip"
+  filename      = "${path.module}/lambda_functions/create_url.zip"
 
   source_code_hash = data.archive_file.create_url.output_base64sha256
 
@@ -38,7 +38,7 @@ resource "aws_lambda_function" "retrieve_url" {
   handler       = "retrieve_url.lambda_handler"
   runtime       = "python3.13"
   role          = aws_iam_role.lambda_role.arn
-  filename      = "./lambda_functions/retrieve_url.zip"
+  filename      = "${path.module}/lambda_functions/retrieve_url.zip"
 
   source_code_hash = data.archive_file.retrieve_url.output_base64sha256
 
