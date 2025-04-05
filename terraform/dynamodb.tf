b6aa5795fd9d4c45325641e6d97cc98d77c53dee
kytan2992 <kytan2992@gmail.com>
2025-04-05 16:51:09 +0800

    added dynamodb table

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -14,32 +14,10 @@ resource "aws_dynamodb_table" "url_table" {
   }
 
   attribute {
-    name = "created_at"
-    type = "S"
-  }
-
-  attribute {
-    name = "ttl"
-    type = "N"
-  }
-
-  attribute {
-    name = "short_url"
-    type = "S"
-  }
-
-  attribute {
     name = "hits"
     type = "N"
   }
 
-  # attribute {
-  #   name = "analytics"
-  #   type = "M"  # Map type for nested data (User-Agent, source IP, etc.)
-  # }
-  
-  ttl {
-    attribute_name = "ttl"
-    enabled        = true  # Enable TTL functionality for automatic deletion
-  }
 }
+  
+  
