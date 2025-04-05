a111ee0047a857b8dd01ac3955a4798ad99d7897
kytan2992 <kytan2992@gmail.com>
2025-04-05 18:37:56 +0800

    added lambda functions

---
+++ terraform/iam.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role" "lambda_role" {
+  name               = "${local.resource_prefix}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json  
+}
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "${local.resource_prefix}-lambda-policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = data.aws_iam_policy_document.lambda_policy.json
+}
+
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "xray:PutTraceSegments",
+      "xray:PutTelemetryRecords",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem"
+    ]
+    resources = ["*"] # You can later restrict this to your table ARN
+  }
+}
