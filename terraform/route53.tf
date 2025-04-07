4f3aed8b9ee6ac9ad127437f7a0d6d96e4cb0ffa
kytan2992 <kytan2992@gmail.com>
2025-04-07 17:21:56 +0800

    fixed wrong syntaxes

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -4,8 +4,8 @@ resource "aws_route53_record" "api" {
   type    = "A"
 
   alias {
-    name                   = aws_api_gateway_domain_name.shortener.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.shortener.cloudfront_zone_id
+    name                   = aws_api_gateway_domain_name.shortener.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.shortener.regional_zone_id
     evaluate_target_health = true
   }
 }
